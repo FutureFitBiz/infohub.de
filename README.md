cdba82473b6ccf23d150acff5efbd451b3bac749
martinjrich <78957354+martinjrich@users.noreply.github.com>
2026-02-26 12:30:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Future-Fit Business Benchmark Info Hub
+
+This repository is managed by Future-Fit Foundation.
---
+++ book.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+/*
+  // Section anchors - superseded by new anchor_sections: TRUE feature in _output.yml
+  $('.section h1, .section h2, .section h3, .section h4, .section h5, .section h6').each(function() {
+    anchor = '#' + $(this).parent().attr('id');
+    $(this).addClass("hasAnchor").prepend('<a href="' + anchor + '" class="anchor"></a>');
+  });
+*/
+/*
+  // Attempt to send bibliography links to another window - not working!
+  $('.csl-entry .csl-right-inline a').each(function() {
+    $(this).setAttribute('target', '_blank');
+  });
+*/
+});
